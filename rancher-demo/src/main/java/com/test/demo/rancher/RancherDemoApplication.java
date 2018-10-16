df0502da24f48d705456baef32c6966f67aca527
yuanjunnan <yuanjunnan@jd.com>
2018-10-16 13:23:02 +0800

    rancher demo

---
+++ rancher-demo/src/main/java/com/test/demo/rancher/RancherDemoApplication.java
@@ -0,0 +1,12 @@
+package com.test.demo.rancher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RancherDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RancherDemoApplication.class, args);
+    }
+}
